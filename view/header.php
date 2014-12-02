ed603ac068545cab762aeae8eae9e31716a4ad0c
jonathan.jung <jung.jonathan1@gmail.Com>
2014-12-02 14:30:00 +0100

    Prise en compte du fichier javascript dans le header des fichiers html

--- view/header.php
+++ view/header.php
@@ -4,5 +4,6 @@
         <?php echo '<title>'.$titre.'</title>';?>
         <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 		<link rel="stylesheet" href="style.css" />
+		  <script src="public/js/gestionTank.js"></script>
     </head>
     <body>
