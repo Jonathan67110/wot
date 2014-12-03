975777a45014bcaf76e253b3621db2f8ece7a561
Jonathan Jung <jonathan.jung@open-groupe.com>
2014-12-03 09:43:37 +0100

    Correction de la redirection vers phpmyadmin suivant l'hébergeur du site

--- view/navigation.php
+++ view/navigation.php
@@ -3,7 +3,19 @@
 			<a href=".">Recherche</a>
 			<a href=".?page=gestionTank">Gestion</a>
 			<a href=".">Connection</a>
-			<a href="http://localhost/phpmyadmin/#PMAURL-2:db_structure.php?db=wot&table=&server=1&target=&token=c7ff5ec32123e7f060068a5604702730" target ="_blank">Bdd Wot</a>
+			<?php
+				//Redirection de l'adresse vers phpmyadmin, en fonction de la machine qui héberge l'application:
+				if(isset($_SERVER['SERVER_ADDR']) && strstr($_SERVER['SERVER_ADDR'], '192.168.'))
+				{
+					$phpMyAdminLink = $_SERVER['SERVER_ADDR'];
+				}
+				else
+				{
+					$phpMyAdminLink = '';				
+				}
+				
+				echo '<a href="'.$phpMyAdminLink.'/phpmyadmin/#PMAURL-2:db_structure.php?db=wot&table=&server=1&target=&token=c7ff5ec32123e7f060068a5604702730" target ="_blank">Bdd Wot</a>';
+			?>
 			<a href="http://worldoftanks.eu/encyclopedia/vehicles/" target="_blank">Tankopédia</a>
 			<a href="aide/" target="_blank">Aide</a>
 			
