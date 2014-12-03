4afe82fa17fa2f741d4ce7f0b0d57505037caee3
Esekios2 <jung.jonathan1@gmail.com>
2014-12-04 00:56:45 +0100

    Correction de l'adresse dirigeant vers phpmyadmin

--- view/navigation.php
+++ view/navigation.php
@@ -14,7 +14,7 @@
 					$phpMyAdminLink = '';				
 				}
 				
-				echo '<a href="'.$phpMyAdminLink.'/phpmyadmin/#PMAURL-2:db_structure.php?db=wot&table=&server=1&target=&token=c7ff5ec32123e7f060068a5604702730" target ="_blank">Bdd Wot</a>';
+				echo '<a href="http://'.$phpMyAdminLink.'/phpmyadmin/#PMAURL-2:db_structure.php?db=wot&table=&server=1&target=&token=c7ff5ec32123e7f060068a5604702730" target ="_blank">Bdd Wot</a>';
 			?>
 			<a href="http://worldoftanks.eu/encyclopedia/vehicles/" target="_blank">Tankopédia</a>
 			<a href="aide/" target="_blank">Aide</a>
