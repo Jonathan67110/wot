52fa1e0c6143f28076e673613bcdfaff43f2c964
jonathan.jung <jung.jonathan1@gmail.Com>
2014-12-08 23:56:07 +0100

    Correction de lien : IL FAUT GARDER LES MAJUSCULE COMME DANS LES DOSSIERS

--- view/navigation.php
+++ view/navigation.php
@@ -17,6 +17,6 @@
 				echo '<a href="http://'.$phpMyAdminLink.'/phpmyadmin/#PMAURL-2:db_structure.php?db=wot&table=&server=1&target=&token=c7ff5ec32123e7f060068a5604702730" target ="_blank">Bdd Wot</a>';
 			?>
 			<a href="http://worldoftanks.eu/encyclopedia/vehicles/" target="_blank">Tankopédia</a>
-			<a href="aide/index.html" target="_blank">Aide</a>
+			<a href="Aide/" target="_blank">Aide</a>
 			
 		</nav>
