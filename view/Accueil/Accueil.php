538109cecc0e0f4f707e2a173cf0dcd7a1179ac8
Jonathan Jung <jonathan.jung@open-groupe.com>
2014-12-16 11:42:59 +0100

    Mise à jour des to do list

--- view/Accueil/Accueil.php
+++ view/Accueil/Accueil.php
@@ -11,21 +11,27 @@
 		
 			<h2>Choix de la recherche</h2>
 				<section>				
-					<h3>Message du jour</h3>
+					<h3>Reste à faire au <?php echo date('d.m.Y');?></h3>
 						<article>
 							<p>
-								<ul>
-									<li>Remettre en forme, avec du javascript, les copier-coller dans la gestion des tanks :
-										<ul>
-											<li>Le poids et la charge admise : convertir le séparateur dans le format chiffre de la bdd mysql</li>
-											<li>Ne conserver que les chiffres quand on copie colle un poids/chassis ( vérifier csi c'est déjà le cas, sinon, l'implémenter)</li>
-										</ul>
-									</li>	
-									<li>Faire que le tier est saisie automatique (clé étrangère qui redirige vers les tiers) => on n'affiche plus du tout de tier chiffre dans la gestion des tanks</li>
-									<li>Mettre un label à la place d'un input sur le textbox du nombre d'équipage</li>
+								<ul>	
+									<li>µAjouter le n° de version de la version du char affiché</li>
+									<li>Reste à faire les lourds chinois chez les chinois</li>
+									<li>Faire que le tier est saisie automatique (clé étrangère qui redirige vers les tiers) => on n'affiche plus du tout de tier chiffre dans la gestion des tanks. Il faudra également veiller à supprimer les fonctions de conversion, superflues(chasse-les, elles seront éparpillées, mouhahahaha)</li>
+									<li>Ajouter un textarea qui traitera un copié collé automatiquement pour insérer des éléments en avance, histoire de gagner du temps</li>
 									<li>Faire une section canon dans un sous-menu => voir le menu de navigation d'Alain</li>
 									<li>Mettre à jour les canons japonais ( tant que c'est frais)</li>
 									<li>N'afficher la page recherche wot que lorsqu'on clique sur recherche. Mettre sur la page d'accueil les essages du site = la to do list par exemple + Les évolutions.</li>
+									<li>N'afficher la page recherche wot que lorsqu'on clique sur recherche. Mettre sur la page d'accueil les essages du site = la to do list par exemple + Les évolutions.</li>
+								</ul>
+							</p>
+						</article>				
+					<h3>Fait </h3>
+						<article>
+							<p>				
+								<h4>Fait </h4>
+								<ul>
+									Amélioration de la page gestion de tank: ajout de js pour remettre en forme, de mofi du phpp, etc...
 								</ul>
 							</p>
 						</article>					
