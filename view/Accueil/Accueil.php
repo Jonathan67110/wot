c1f08caf5a8d9d05681669f63bb7ad8b9abb99a4
Esekios2 <jung.jonathan1@gmail.com>
2015-04-13 23:08:32 +0200

    Modif des remarques + maj bdd

--- view/Accueil/Accueil.php
+++ view/Accueil/Accueil.php
@@ -10,31 +10,19 @@
         <h1>Accueil</h1>
 		
 			<h2>Choix de la recherche</h2>
-				<section>			
-					<h3>Fait </h3>
-						<article>
-							<p>				
-								<h4>Fait le 19/12/2014: </h4>
-								<ul>	
-									<li>b-1 Corriger la disparition du réglage "type de char" : Corrigé</li>
+				<section>						
+					<h3>Reste à faire au <?php echo date('d.m.Y');?></h3>	
+						<p>
+							<?php 
+								$remarquesTotales = 18;
+								$remarqueEnCours = 14;
+							?>
+								<ul>
+									<li><?php echo $remarquesTotales;?> remarques au total</li>
+									<li><?php echo $remarqueEnCours;?> remarques en cours</li>
+									<li><?php echo $remarquesTotales - $remarqueEnCours;?> remarques traités</li>
 								</ul>
-							</p>
-							<p>				
-								<h4>Fait le 17/12/2014: </h4>
-								<ul>	
-									<li>Ajout d'un textarea pour copier-coller des infos</li>
-								</ul>
-							</p>
-							<p>				
-								<h4>Fait le 16/12/2014: </h4>
-								<ul>	
-									<li>Amélioration de la page gestion de tank: ajout de js pour remettre en forme, de mofi du phpp, etc...</li>
-									<li>Ajout d'un pied de page</li>
-									<li>Ajout de la page de connection</li>
-								</ul>
-							</p>
-						</article>				
-					<h3>Reste à faire au <?php echo date('d.m.Y');?></h3>		
+						</p>	
 						<h4>Important et urgent</h4>
 						<article>	
 							<p>
@@ -49,11 +37,10 @@
 						<h4>Important et Non-urgent</h4>
 						<article>	
 							<p>
-								<ul>		
-									<li>2) Corriger l'affichage du tier chiffre qui est en latin, lors de l'envoi du copier-coller</li>
+								<ul>
 									<li>4) Mettre à jour les canons et les membres d'équipage des chars français</li>
-									<li>5) Idem légers britannique</li>
-									<li>9) Faire que le tier est saisie automatique (clé étrangère qui redirige vers les tiers) => on n'affiche plus du tout de tier chiffre dans la gestion des tanks. Il faudra également veiller à supprimer les fonctions de conversion, superflues(chasse-les, elles seront éparpillées, mouhahahaha)</li>
+									<li>5) Mettre à jour les canons et les membres d'équipage des chars britanniques</li>
+									<li>9) Faire que le tier est saisie automatiquement (clé étrangère qui redirige vers les tiers) => on n'affiche plus du tout de tier chiffre dans la gestion des tanks. Il faudra également veiller à supprimer les fonctions de conversion, superflues(chasse-les, elles seront éparpillées, mouhahahaha)</li>
 									<li>10) Faire une section canon dans un sous-menu => voir le menu de navigation d'Alain : voir à faire quelque chose de réutilisable pour chaque élément</li>
 									<li>11) Mettre à jour les canons japonais ( tant que c'est frais)</li>
 									<li></li>
@@ -75,11 +62,41 @@
 									<li>7) Ajouter un élément pour ne modifier qu'un élément sur un char au lieu de tout le char : permettra de revenir à cet élément dans la page html (#idelement) </li>
 									<li>14) Améliorer la connexion</li>
 									<li>13) Pour les liaisons Tank-Canon, ajouter des paramètres qui inclueront les obus, la dépreciation, la vitesse de visée, ... qui pourraient dépendre du char</li>
+									<li>17) Ajouter dans tankliaison un paramètre donnant l'id du canon prédécesseur. Voir à ajouter un Id tank-canon pour cela, histoire </li>
+									<li>18) Ajouter une vérification quand une liaison est cassé par la suppression d'un canon</li>
 								</ul>
 							</p>
-						</article>					
+						</article>			
+					<h3>Fait </h3>
+						<article>
+							<p>				
+								<h4>Fait le 12/04/2015: </h4>
+								<ul>			
+									<li>2) Corriger l'affichage du tier chiffre qui est en latin, lors de l'envoi du copier-coller</li>
+								</ul>
+							</p>
+							<p>				
+								<h4>Fait le 19/12/2014: </h4>
+								<ul>	
+									<li>b-1 Corriger la disparition du réglage "type de char" : Corrigé</li>
+								</ul>
+							</p>
+							<p>				
+								<h4>Fait le 17/12/2014: </h4>
+								<ul>	
+									<li>Ajout d'un textarea pour copier-coller des infos</li>
+								</ul>
+							</p>
+							<p>				
+								<h4>Fait le 16/12/2014: </h4>
+								<ul>	
+									<li>Amélioration de la page gestion de tank: ajout de js pour remettre en forme, de mofi du phpp, etc...</li>
+									<li>Ajout d'un pied de page</li>
+									<li>Ajout de la page de connection</li>
+								</ul>
+							</p>
+						</article>			
 				</section>
-				
 <?php 
 	/* footer */
 	include_once('view/footer.php');
